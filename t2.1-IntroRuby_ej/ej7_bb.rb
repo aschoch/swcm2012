3f5e315f5707473acabc8a58dd6834384ae32335
aschoch <alfredoschoch@gmail.com>
2012-02-20 21:53:23 +0100

    Update t2.1-IntroRuby_ej/ej7_bb.rb

--- t2.1-IntroRuby_ej/ej7_bb.rb
+++ t2.1-IntroRuby_ej/ej7_bb.rb
@@ -1,6 +1,6 @@
 require_relative 'ej6_book'
 
-b = Book.new('El Hobbit', 'Novela .......')
+b = Book.new('El Hobbit', 'Novela .......', 15)
 puts "Titulo: #{b.title}, Descripcion: #{b.description}"
 
 
@@ -8,5 +8,9 @@ b.title = 'EL Hobbit 2'
 puts "Titulo: #{b.title}, Descripcion: #{b.description}"
 
 puts  "",  b.inspect,  b.to_s
+
+puts "#{b.title} esta disponible" if b.stock?
+
+puts "#{b.title} no esta disponible" if !b.stock?
 
   
