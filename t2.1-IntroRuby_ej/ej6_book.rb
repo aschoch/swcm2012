73ca99d7d9ceedf0e165eb8a4c0a46403494b400
aschoch <alfredoschoch@gmail.com>
2012-02-20 21:51:38 +0100

    Update t2.1-IntroRuby_ej/ej6_book.rb

--- t2.1-IntroRuby_ej/ej6_book.rb
+++ t2.1-IntroRuby_ej/ej6_book.rb
@@ -1,15 +1,22 @@
 class Book < Object
   
-  def initialize(title, description)
+  def initialize(title, description, stock)
     @title = title
     @description = description
+    @stock = stock
   end
   
   attr_accessor    :title
-  attr_reader       :description
+  attr_reader      :description
+  attr_accessor     :stock 
   
   def upper_case_title
     return @title.uppercase
+  end
+  
+  def stock?
+    return true if stock!=0
+    return false if stock==0
   end
   
   AGOTADO = 'Libro agotado'
